23ac09a7e97a0b6937207028edcb265e8fa87b69
Stephanie Smith <stephanie-k-smith@uiowa.edu>
2016-02-04 15:27:25 -0600

    added some test functions

--- ThirdBox/ThirdBox.ino
+++ ThirdBox/ThirdBox.ino
@@ -5,6 +5,7 @@ int tempPin=0; //(analog) connected to A0
 short buttonPin=2; //don't worry about this button yet
 //later add wifi pins
 int temperature=0;
+int ASCIIconversion=48;
 void setup() 
 {
   for(int i=0; i<LEDS_NUM; i++) //configure LEDs
@@ -13,29 +14,55 @@ void setup()
   }
   pinMode(A0, INPUT); //configure temp sensor input
   pinMode (buttonPin, INPUT);
-  Serial.begin(9600); //for testing
+  Serial.begin(9600); //FOR TESTING
 }
-
 void loop() 
 {
-  temperature=readTempSensor();
-  //TEST!
-  Serial.print("TEMPRATURE = ");
-  Serial.print(temperature);
-  Serial.print("*C");
-  Serial.println();
-  delay(1000);
+  //temperature=readTempSensor();
+  int temperature= generateTempTest();
+  displayTempTest(temperature);
 
 }
 void intToBinary(int temperature)
 {
+  byte char_variable = (byte) temperature;
 }
 void displayTemperature()
 {
 }
+/**
+ * This function reads in the analog input and converts this input
+ * to a temperature (in degrees celsius) by using this ADC formula:
+ * temperature = (reference voltage/resolution) * (analog voltage measured) * 100
+ * temperature = (analog voltage measured) * 100 * (5.00 V/1024)
+ * temperature = (analog voltage measured) * 0.48828125
+ */
 int readTempSensor()
 {
-  int temperature = analogRead(tempPin);
+  int temperature = analogRead(tempPin); //casting input to integer
   temperature = temperature * 0.48828125;
   return temperature;
+}
+/**
+ * This function displays the temperature (in degrees C) to the serial monitor. 
+ * This function is for TESTING PURPOSES ONLY
+ */
+void displayTempTest(int temperature)
+{
+  Serial.print("Integer Temperature = "); //prints out the temperature to the serial prompt
+  Serial.print(temperature);
+  Serial.println("*C");
+  delay(500); //delays 1/2 a second
+}
+/**
+ * This function prompts a user to enter temperature values.
+ */
+int generateTempTest()
+{
+  Serial.print("enter temp in degrees C: ");
+  while (Serial.available() <= 0){} //get the number of bytes available for reading from the serial port
+  int incomingTemp = Serial.readString().toInt(); // read the incoming string from the user and converts it an integer
+  Serial.println(); 
+  return incomingTemp;
+  
 }
