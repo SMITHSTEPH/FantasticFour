92a5168f7c11a37ae983908c153d41c698d5ff4b
Stephanie Smith <stephanie-k-smith@uiowa.edu>
2016-02-05 13:01:32 -0600

    working on serial comm and storing tempdata

--- ThirdBox/ThirdBox.ino
+++ ThirdBox/ThirdBox.ino
@@ -1,51 +1,59 @@
-
+//still needs to accumulate 300 sec worth of data
+//need to have button interrupts
+//need to have serial interrupts
+//need to connect to hardware
 const short LEDPins[] = {5,6,8,9,11,12,13}; //need to have 7 LEDs
 const short LEDS_NUM =7;
 const short buttonPin=2; //don't worry about this button yet
 const int tempPin=0; //(analog) connected to A0
-int temperature=0;
+//globals
+short temperature=0;
+bool pc = false;  
 
 //later add wifi pins
 void setup() 
 {
-  for(int i=0; i<LEDS_NUM; i++) //configure LEDs
-  {
-    pinMode(LEDPins[i], OUTPUT);
-  }
+  for(int i=0; i<LEDS_NUM; i++) { pinMode(LEDPins[i], OUTPUT);} //configuring LED pins
   pinMode(A0, INPUT); //configure temp sensor input
   pinMode (buttonPin, INPUT);
-  
-  Serial.begin(9600); //FOR TESTING
+  Serial.begin(9600); //for serial communication with PC
 }
 void loop() 
-{
-  //temperature=readTempSensor();
-  int temperature= generateTempTest();
-  fastTempDisplay(temperature);
-  //displayTempTest(temperature);
-  //String test=intToBitString(temperature);
-  //Serial.println(test);
+{ 
+  temperature=readTempSensor();
+  if(pc){sendData(temperature);}
+  else{storeTempData(temperature);}
 }
 /**
- * This function converts the integer temperature into a bit String. 
- * -- if the third box is too slow revise this function 
+ * ARMOND & JOE
+ * Interrupt triggered function
+ * Called when data is available. Use Serial.read() to capture this data.
+ * Here are the messages the arduino could receive from the PC
+ *  0 == PC ready
+ *  1 == push third box button 
+ *  2 == release third box button
+ *  3 == PC no longer ready
  */
-String intToBitString(int temperature)
+void serialEvent()
 {
-  if(temperature <= 63 || temperature >= -64) //possible range of values the the 7 LEDs can display
-  {
-    int startingIndex=0;
-    String wholeByte=String((byte)temperature, BIN); //converting the temperature to a bit string; the size of the bit string depends on where the last 1 is ==> ie if the MSB is 1,
-                                         //then the size of the bit string is 8
-    if(wholeByte.length()==8){return wholeByte.substring(1, 8);} //return the last 7 bits
-    else
-    {
-      int newLength=7-wholeByte.length();
-      for(int i = 0; i < newLength; i++){wholeByte= "0" + wholeByte;} //adding the extras 0s
-      return wholeByte;
-    }
-  }
-  else{return "error";} //trying to display a temp val it can't
+  short pcData = Serial.readString().toInt();
+  Serial.println("interrupt data is: " + String(pcData, DEC)); //for testing
+  if(pcData==0){pc=true;}
+  else if(pcData==1){fastTempDisplay(temperature);}
+  else if(pcData==2){writeAllLEDS(0);}
+  else if(pcData==3){pc=false;}
+}
+void sendData(short data) //casting to a short b/c this information can be represented in 7 bits
+{
+  //empty queue if not empty
+  Serial.println(data); //sending the data to the PC
+}
+/**
+ * This function stores the temperature into a queue if it the PC is not available
+ * We only was to store 300 seconds worth of data
+ */
+void storeTempData(short temp)
+{
 }
 /**
  * This function reads in the analog input and converts this input
@@ -60,33 +68,10 @@ int readTempSensor()
   temperature = temperature * 0.48828125;
   return temperature;
 }
-/**
- * This function displays the temperature (in degrees C) to the serial monitor. 
- * This function is for TESTING PURPOSES ONLY
- */
-void displayTempTest(int temperature)
-{
-  Serial.print("Integer Temperature = "); //prints out the temperature to the serial prompt
-  Serial.print(temperature);
-  Serial.println("*C");
-  //delay(500);
-}
-/**
- * This function prompts a user to enter temperature values, so that this program can be debugged
- * without having a temperature sensor
- */
-int generateTempTest()
-{
-  Serial.print("enter temp in degrees C: ");
-  while (Serial.available() <= 0){} //get the number of bytes available for reading from the serial port
-  int incomingTemp = Serial.readString().toInt(); // read the incoming string from the user and converts it an integer
-  Serial.println(); 
-  return incomingTemp;
- }
  /**
   * This functin lights up the LED display quickly
   */
- void fastTempDisplay(int temp)
+ void fastTempDisplay(short temp)
  {
     int j=6;
     for(int i=0; i<LEDS_NUM; i++)
@@ -94,15 +79,6 @@ int generateTempTest()
       digitalWrite(LEDPins[j], (0x01<<i)&((byte)temp)); //better way to do this
       j--;
     }
-
-    /*digitalWrite(LEDPins[6], 0x01&((byte)temp)); //hard coded values
-    digitalWrite(LEDPins[5], 0x02&((byte)temp));
-    digitalWrite(LEDPins[4], 0x04&((byte)temp));
-    digitalWrite(LEDPins[3], 0x08&((byte)temp));
-    digitalWrite(LEDPins[2], 0x10&((byte)temp));
-    digitalWrite(LEDPins[1], 0x20&((byte)temp));
-    digitalWrite(LEDPins[0], 0x40&((byte)temp));*/
-    //digitalWrite(LEDPins[6], 0x00 & ((byte)temp)); //TEST --> turning off  built in LED
  }
  /**
   * function flashes LEDS on and off
@@ -125,3 +101,47 @@ int generateTempTest()
       digitalWrite(LEDPins[i], mode);
     }
  }
+ /**
+ * This function prompts a user to enter temperature values, so that this program can be debugged
+ * without having a temperature sensor
+ */
+int generateTempTest()
+{
+  Serial.print("enter temp in degrees C: ");
+  while (Serial.available() <= 0){} //get the number of bytes available for reading from the serial port
+  int incomingTemp = Serial.readString().toInt(); // read the incoming string from the user and converts it an integer
+  Serial.println(); 
+  return incomingTemp;
+ }
+ /**
+ * This function displays the temperature (in degrees C) to the serial monitor. 
+ * This function is for TESTING PURPOSES ONLY
+ */
+void displayTempTest(int temperature)
+{
+  Serial.print("Integer Temperature = "); //prints out the temperature to the serial prompt
+  Serial.print(temperature);
+  Serial.println("*C");
+  //delay(500);
+}
+/**
+ * This function converts the integer temperature into a bit String. 
+ * -- if the third box is too slow revise this function  --> for testing
+ */
+String intToBitString(int temperature)
+{
+  if(temperature <= 63 || temperature >= -64) //possible range of values the the 7 LEDs can display
+  {
+    int startingIndex=0;
+    String wholeByte=String((byte)temperature, BIN); //converting the temperature to a bit string; the size of the bit string depends on where the last 1 is ==> ie if the MSB is 1,
+                                         //then the size of the bit string is 8
+    if(wholeByte.length()==8){return wholeByte.substring(1, 8);} //return the last 7 bits
+    else
+    {
+      int newLength=7-wholeByte.length();
+      for(int i = 0; i < newLength; i++){wholeByte= "0" + wholeByte;} //adding the extras 0s
+      return wholeByte;
+    }
+  }
+  else{return "error";} //trying to display a temp val it can't
+}
