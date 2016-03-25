cf3d6ebf41e156671bfa569f6c3c134fe989446e
Joe Hallman <gohawkeyes74@yahoo.com>
2016-03-24 23:59:04 -0500

    Joe's commits

---
+++ Lab2Armond/ADconverter/ADconverter.ino
@@ -0,0 +1,14 @@
+
+int val = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  pinMode(A3, INPUT);
+  val = analogRead(A3);
+  Serial.println(val);
+}
---
+++ Lab2Armond/crcCalc/CRCcalc.ino
@@ -0,0 +1,41 @@
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int POLY = 0x8408;
+  int MASK = 0x0001;
+
+  int crc = 0xFFFF;
+  int data = 0x54;
+
+  String hexDataString = String(data, HEX);
+  int stop = (hexDataString.length())/2;
+  String stopString = String(stop, HEX);
+    
+    Serial.print(data);
+    Serial.print(hexDataString);
+    Serial.print(stop);
+    Serial.print(stopString);
+    
+    for(int i = 0; i < stop*8; i++)
+    {
+      int databit = data & MASK;
+      int crcbit = data & MASK;
+      crc>>=1;
+      if((databit ^ crcbit) == 1)
+      {
+        crc ^= POLY;
+      }
+      
+      data >>= 1;
+    }
+    
+    crc ^= 0xFFFF;
+    String crcString = String(crc, HEX);
+    
+    Serial.print("CRC = " + crc);
+    Serial.print("CRC = " + crcString);
+}
