9585b223bcf1df21cebf3205d3c85f9921f9ab97
Stephanie Smith <stephanie-k-smith@uiowa.edu>
2016-03-28 11:33:49 -0500

    started nzri

--- Lab2Armond/ZeroCrossAlgorithm/ZeroCrossAlgorithm.ino
+++ Lab2Armond/ZeroCrossAlgorithm/ZeroCrossAlgorithm.ino
@@ -5,38 +5,52 @@
 /*globals*/
 
 /*for zero-crossing algorithm*/
-short inTone; //tone coming in currently
-short prevTone; //previous tone that came in (need to keep track of in order to decode)
-boolean TMR1_flag;
+short inTone; //tone coming in currently 
+short prevTone=0; //previous tone that came in (need to keep track of in order to decode)
+bool startTrans=false; //set to true the first time transmission begins
+//boolean TMR1_flag;
 /*for arduino*/
-short inPin=0; //signal will come in on this pin -> analog or digital pin?
-
+short inPin=2; //using digital pin 8
+String packets="";
+int i=0;
+int val=3;
 
 void setup() 
 {
   pinMode(inPin, INPUT); 
+  //digitalWrite(inPin,HIGH);  // enable pullup resistor
   attachInterrupt(digitalPinToInterrupt(inPin), determineFrequency, CHANGE);
   Serial.begin(9600); //configure baudrate for serial port
-  Timer1.initialize(295); //argument is period in microseconds --> 590/2
-  //589 us is > the threshold period
+  Timer1.initialize(294); //argument is period in microseconds --> 588/2
+  //if more than two ticks have passed, then it beyond the 1700 Hz threshold
 }
 
 void loop() 
 {
-  zeroCrossingAlgorithm();
-  
+    if(inTone==0 &&  prevTone==0)
+    {
+    }
 }
 /**function will display HDLC packet**/
 void displayPacket(void)
 {
 }
 
-void determineFrequency(void) //part of zero crossing algorithm
+void determineFrequency() //part of zero crossing algorithm
 {
+  //prevTone=inTone; //get prevTone
+  Timer1.restart();
+  unsigned long curTime=0;
+  inTone=HIGH;
+  while(curTime<588){curTime=Timer1.read();} //doesn't really account for noise
+  inTone=LOW;
 }
 /**function will collect and decode signal**/
+
 void zeroCrossingAlgorithm(void)
 {
+
+  
 }
 /**
  * this function calcutates the CRC
@@ -49,9 +63,19 @@ void zeroCrossingAlgorithm(void)
 bool isPacketError(void)
 {
 }
-
+/*void serialEvent()
+{
+  while (Serial.available()) 
+  {
+   char inChar = (char)Serial.read();
+   Serial.print("data:");
+   Serial.write(inChar);
+   Serial.println();
+   Serial.print(packets);
+  }
+}*/
 //might not need this
-void serialEvent() {
+/*void serialEvent() {
   while (Serial.available()) {
 
     int inTone = (int)Serial.read();
@@ -88,5 +112,5 @@ void serialEvent() {
         }
       }
     }
-  }
-}
+}*/
+
